ba867a927ae37028f5aaa33b611e12ebfeb90360
unknown <hijulia259@gmail.com>
2023-07-22 12:33:19 -0300

    campo texto

---
+++ ReactJS/Organo/organo/src/App.js
@@ -0,0 +1,17 @@
+
+import Banner from './componentes/Banner';
+import CampoTexto from './componentes/CampoTexto';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      <CampoTexto label="Nome"/>
+      <CampoTexto label="Cargo"/>
+      <CampoTexto label="Imagem"/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/Organo/organo/src/componentes/CampoTexto/index.jsx
@@ -0,0 +1,13 @@
+import "./CampoTexto.css"
+
+
+const CampoTexto = () => {
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input placeholder="Digite o seu nome"/>
+        </div>
+    )
+}
+
+export default CampoTexto
